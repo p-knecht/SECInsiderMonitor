0201bfd0fc5cc37f1b44064ebdf6f1dcb6cfa857
Pascal Knecht <33715220+p-knecht@users.noreply.github.com>
2025-02-16 23:41:05 +0100

    Refactor: Update logging mechanism to use log4js instead of winston and add file logging support

--- data-fetcher/logger.ts
+++ data-fetcher/logger.ts
@@ -1,17 +1,37 @@
-import * as winston from 'winston';
+import log4js from 'log4js';
 
-// create a logger to console with a timestamp and log level in uppercase next to the message
-const logger: winston.Logger = winston.createLogger({
-  level: process.env.LOG_LEVEL || 'info', // use LOG_LEVEL environment variable if set, otherwise default to 'info'
-  format: winston.format.combine(
-    winston.format.timestamp(),
-    winston.format.printf(
-      ({ timestamp, level, message }) => `[${timestamp}] [${level.toUpperCase()}] ${message}`, // show timestamp and log level in uppercase next to the message
-    ),
-  ),
-  transports: [
-    new winston.transports.Console(), // print logs to console to allow log collection by docker
-  ],
+// create a logger which logs to console and file
+log4js.configure({
+  appenders: {
+    console: {
+      // create configuration for console logging based on defined pattern
+      type: 'console',
+      layout: {
+        type: 'pattern',
+        pattern: '%[%d %z [%p]%] %m', // date, process id, log level, message; first three are colored based on log level
+      },
+    },
+    file: {
+      // create configuration for file logging with daily rotation, 30 days retention and custom pattern
+      type: 'dateFile',
+      filename: 'logs/datafetcher.log',
+      pattern: 'yyyy-MM-dd',
+      alwaysIncludePattern: true, // make sure the pattern is always included in the log file name, even on the first day
+      keepFileExt: true, // make sure the log file extension is kept
+      fileNameSep: '-',
+      numBackups: 30, // keep 30 days of logs
+      layout: {
+        type: 'pattern',
+        pattern: '%d %z [%p] %m', // date, process id, log level, message
+      },
+    },
+  },
+  categories: {
+    default: { appenders: ['console', 'file'], level: process.env.LOG_LEVEL || 'info' },
+  },
 });
+
+// create logger based on the configuration
+const logger = log4js.getLogger();
 
 export default logger;
