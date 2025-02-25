8c5a24cfaad3bf435c39c333ae896baf29a1a439
Pascal Knecht <33715220+p-knecht@users.noreply.github.com>
2025-02-25 07:47:00 +0100

    Fix: replace title on auth layout

--- app-server/app/auth/layout.tsx
+++ app-server/app/auth/layout.tsx
@@ -7,12 +7,12 @@ const AuthLayout = ({
   return (
     <div className="bg-muted flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
       <div className="flex w-full max-w-sm flex-col gap-7">
-        <a href="#" className="flex items-center gap-3 self-center font-medium" tabIndex={-1}>
+        <span className="flex items-center gap-3 self-center font-medium" tabIndex={-1}>
           <Image src="/logo_192.png" alt="Logo" width={48} height={48} />
           <h1 className="text-3xl font-semibold text-gray-600 drop-shadow-md pb-1">
             SECInsiderMonitor
           </h1>
-        </a>
+        </span>
         <div className="flex flex-col gap-6">{children}</div>
       </div>
     </div>
