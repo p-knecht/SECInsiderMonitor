c6af7aa3296956edb307d7066b12fbe9b614d6af
Pascal Knecht <33715220+p-knecht@users.noreply.github.com>
2025-03-02 14:12:37 +0100

    fix: prevent execution of UsersPage processing for non-admin users

--- app-server/app/(internal)/admin/users/page.tsx
+++ app-server/app/(internal)/admin/users/page.tsx
@@ -5,6 +5,7 @@ import { DataTable } from '@/components/data-table/data-table';
 import { columns, UserColumn } from './columns';
 import { dbconnector } from '@/lib/dbconnector';
 import { buildUserTableFilter } from '@/lib/tablefilter';
+import { currentRole } from '@/lib/auth';
 
 interface UsersPageProps {
   searchParams: Promise<{
@@ -17,43 +18,55 @@ interface UsersPageProps {
 }
 
 export default async function UsersPage({ searchParams: searchParams }: UsersPageProps) {
-  const retrievedSearchParams = await searchParams;
-
-  // Handle pagination parameters
-  let page = parseInt(retrievedSearchParams?.page || '1', 10); // Default to page 1
-  const pageSize = parseInt(retrievedSearchParams?.pageSize || '10', 10); // Default to 10 items per page
-
-  // Handle sorting parameters
-  const sortColumn = retrievedSearchParams?.sort || 'createdAt'; // Default to sorting by createdAt
-  const sortOrder = retrievedSearchParams?.order || 'asc'; // Default to ascending order
-
-  // Handle filtering parameters
-  const filter = buildUserTableFilter(retrievedSearchParams);
-
-  const totalCount = await dbconnector.user.count({ where: filter });
-
-  // if page is out of bounds, set it to the last page
-  if (page > Math.ceil(totalCount / pageSize)) {
-    page = Math.ceil(totalCount / pageSize);
-  }
+  // default values
+  let totalCount = 0;
+  let page = 1;
+  let pageSize = 10;
   let users: UserColumn[] = [];
-  if (0 < totalCount) {
-    users = await dbconnector.user.findMany({
-      skip: (page - 1) * pageSize,
-      take: pageSize,
-      select: {
-        // Select only the fields that are needed as table columns
-        email: true,
-        emailVerified: true,
-        createdAt: true,
-        lastLogin: true,
-        role: true,
-      },
-      where: filter,
-      orderBy: {
-        [sortColumn]: sortOrder, // only sort by one column to reduce complexity
-      },
-    });
+
+  // only process request if user is admin
+  if ((await currentRole()) == UserRole.admin) {
+    const retrievedSearchParams = await searchParams;
+    // Handle pagination parameters
+    if (retrievedSearchParams?.page && !isNaN(parseInt(retrievedSearchParams.page, 10)))
+      page = parseInt(retrievedSearchParams.page, 10);
+    if (retrievedSearchParams?.pageSize && !isNaN(parseInt(retrievedSearchParams.pageSize, 10)))
+      pageSize = parseInt(retrievedSearchParams.pageSize, 10);
+
+    // Handle sorting parameters
+    const sortColumn = retrievedSearchParams?.sort || 'createdAt'; // Default to sorting by createdAt
+    const sortOrder = retrievedSearchParams?.order || 'asc'; // Default to ascending order
+
+    // Handle filtering parameters
+    const filter = buildUserTableFilter(retrievedSearchParams);
+
+    totalCount = await dbconnector.user.count({ where: filter });
+
+    // if page is out of bounds, set it to the last page
+    if (page > Math.ceil(totalCount / pageSize)) {
+      page = Math.ceil(totalCount / pageSize);
+    }
+
+    // only fetch data if there are any users
+    if (0 < totalCount) {
+      users = await dbconnector.user.findMany({
+        skip: (page - 1) * pageSize,
+        take: pageSize,
+        select: {
+          // Select only the fields that are needed as table columns
+          email: true,
+          emailVerified: true,
+          createdAt: true,
+          lastLogin: true,
+          role: true,
+        },
+        where: filter,
+        orderBy: {
+          [sortColumn]: sortOrder, // only sort by one column to reduce complexity
+        },
+      });
+      console.log(users);
+    }
   }
 
   return (
